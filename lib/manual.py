f57f7e77c15da1aa77a3fdd355da58c80d509a2c
Mitterpach <patrik@mitterpach.com>
2022-02-08 11:34:14 +0100

    Update manual for better readability

--- lib/manual.py
+++ lib/manual.py
@@ -1,18 +1,21 @@
 def helpMessage():
-    helpMessage = "\
-**:cactus:  KAKTUSKO NÁPOVEDA  :cactus: ** \n\
-Všetky sťažnosti prosím smerujte na @mitterpach#2234 , očakávam že ich bude dosť :sweat_smile: \n\
-Zatiaľ nie je veľa dostupných príkazov, postupne budú pridávané podľa potreby. \n\
-Všetky prvky bota (vzhľad, kompozícia príkazov) sú témou debaty a nie sú určené natvrdo. \n\
-\n\
-**Dostupné príkazy:**\n\
-**`~poll`** `\"otazka ankety\" moznost1 moznost2 moznost3 ...` \n\
-- otázka musí byť vždy v úvodzovkách, možnosti iba jednoslovné \n\
-- maximum možností je 7 \n\
-- použitie: `~poll \"Najlepší predmet prváku?\" ULI PKM TZP` \n\
-\n\
-**`~pollNumbered`** `\"otazka ankety\" pocetMoznosti`\n\
-- vytvorí anketu s otázkou a možnosťami 1 - pocetMoznosti (max 6)\n\
-- použitie: `~pollNumbered \"Koľko predmetov ste úspešne zvládli?\" 6`"
+    helpMessage = """
+
+**:cactus:  KAKTUSKO NÁPOVEDA  :cactus: **
+Všetky sťažnosti prosím smerujte na @mitterpach#2234 , očakávam že ich bude dosť :sweat_smile:  
+Zatiaľ nie je veľa dostupných príkazov, postupne budú pridávané podľa potreby.
+Všetky prvky bota (vzhľad, kompozícia príkazov) sú témou debaty a nie sú určené natvrdo.
+
+Dostupné príkazy:
+**`~poll`** `"otazka ankety" moznost1 moznost2 moznost3 ...` 
+- otázka musí byť vždy v úvodzovkách, možnosti iba jednoslovné
+- maximum možností je 7
+- použitie: `~poll "Najlepší predmet prváku?" ULI PKM TZP`
+
+**`~pollNumbered`** `"otazka ankety" pocetMoznosti`
+- vytvorí anketu s otázkou a možnosťami 1 - pocetMoznosti (max 6)
+- použitie: `~pollNumbered "Koľko predmetov ste úspešne zvládli? 6`
+
+        """
 
     return helpMessage
