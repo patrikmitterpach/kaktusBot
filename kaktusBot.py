2750a547945261b6f15e611ce14b1b5bdeaffd5f
Mitterpach <patrik@mitterpach.com>
2022-02-08 19:34:07 +0100

    Remove non-needed imports from `kaktusBot.py`

--- kaktusBot.py
+++ kaktusBot.py
@@ -2,10 +2,6 @@ import discord
 from os import getenv, system
 from dotenv import load_dotenv as loadDotenv
 
-import lib.polls as polls
-import lib.manual as manual
-from lib.bot import Bot 
-
 loadDotenv('.env')
 token = getenv('discordBotToken')
 
