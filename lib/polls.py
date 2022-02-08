bdc183ad51ee6c6a19b4091f0fb9e01609e23c6a
Mitterpach <patrik@mitterpach.com>
2022-02-08 12:22:42 +0100

    Add more possible reactions to polls

--- lib/polls.py
+++ lib/polls.py
@@ -1,6 +1,6 @@
 def createPollMessage(question, options):
-    emojiReactions = ["0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣"]
-    pollMessage = ["**Anketa:**", str(question)]
+    emojiReactions = ["0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
+    pollMessage = ["**Anketa:**", str(question), "", "**Možnosti:**"]
 
     for idx, char in enumerate(options):
         pollMessage.extend([f'{emojiReactions[idx]}  - {char}'])
