0b1de3680f30e2e592e4466f3e39824b05735ced
Mitterpach <patrik@mitterpach.com>
2022-02-08 12:06:56 +0100

    Add ANSI colored output to `bot.py`

--- lib/bot.py
+++ lib/bot.py
@@ -1,4 +1,4 @@
-from os import system
+from os import listdir
 
 import discord
 from discord.ext import commands
@@ -8,21 +8,27 @@ currentGame   = 'Detroit: Become Human'
 currentStatus = 'idle'
 commandPrefix = '~'
 
+# colors
+yellow = '\033[33m'
+green = '\033[32m'
+reset = '\033[m'
+
 
 class Bot(commands.Bot):
+    async def on_ready(self):
+        print(f'Connected as {yellow}{self.user}{reset}')
+
     @classmethod
     def create(cls):
-        system('clear')
-        print(f'{Bot.user.name} connection created.')
         return cls(
-            command_prefix = commandPrefix,
-            activity = discord.Game(currentGame),
-            status = currentStatus,
-            help_command=None)
-    
+                command_prefix = commandPrefix,
+                activity = discord.Game(currentGame),
+                status = currentStatus,
+                help_command=None )
+        
     def loadExtensions(self):
-        extensionList = [ 'lib.extensions.help',
-                          'lib.extensions.ping' ]
-        for extension in extensionList:
-            self.load_extension(extension)
-            print(f'Loaded {extension}')
+        print('Loading extensions:')
+        for filename in listdir('./lib/extensions'): #loads all files (*.py)
+            if filename.endswith('.py'):
+                self.load_extension(f'lib.extensions.{filename[:-3]}') #loads the file without ".py" 
+                print(f'> Loaded {green}{filename[:-3]}{reset} from lib.extensions.{filename[:-3]}')
