b7afd97829ce3c74993533496260ff32c28c6f27
Mitterpach <patrik@mitterpach.com>
2022-02-08 12:23:37 +0100

    Remove `poll` from commented out text

--- kaktusBot.py
+++ kaktusBot.py
@@ -27,29 +27,6 @@ bot.run(token)
 #         if message.author.id == self.user.id: 
 #             return                            
 
-#         if message.content[0] == '~':
-#             command = message.content.split()[0][1:] # first word without the first char (~)
-#             print(f'{message.author} used {command} in {message.channel}')
-            
-#             if command == 'help':
-#                 await message.channel.send( manual.helpMessage() )
-
-#             # Create a normal poll
-#             elif command == 'poll':
-#                 messageContent = message.content.split('"')
-#                 emojiReactions = ["0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣"]
-#                 pollOptions = messageContent[2][1:].split(" ")
-           
-#                 if len(pollOptions) > len(emojiReactions):
-#                     await message.channel.send(f"**Too many options for the poll!** \
-#                                                \n*(current maxium: {len(emojiReactions)})*")
-#                     return
-
-#                 pollMessage = polls.createPollMessage(messageContent[1], pollOptions)
-#                 sentMessage = await message.channel.send(pollMessage)
-#                 for idx in range(len(pollOptions)):
-#                     await sentMessage.add_reaction(emojiReactions[idx])
-
 #             # Create a poll (answers only integers 1 up to 6)
 #             elif command == 'pollNumbered':
 #                 messageContent = message.content.split('"')
