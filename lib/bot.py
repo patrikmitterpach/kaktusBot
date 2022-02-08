dee739ee17f54febe0b33b6056e2c4780e1c00c9
Mitterpach <patrik@mitterpach.com>
2022-02-08 12:31:38 +0100

    Increase readability for `bot.py`

--- lib/bot.py
+++ lib/bot.py
@@ -3,17 +3,14 @@ from os import listdir
 import discord
 from discord.ext import commands
 
-
 currentGame   = 'Detroit: Become Human'
 currentStatus = 'idle'
 commandPrefix = '~'
 
-# colors
+# ANSI Code Colors
 yellow = '\033[33m'
 green = '\033[32m'
 reset = '\033[m'
-
-
 class Bot(commands.Bot):
     async def on_ready(self):
         print(f'Connected as {yellow}{self.user}{reset}')
@@ -28,7 +25,8 @@ class Bot(commands.Bot):
         
     def loadExtensions(self):
         print('Loading extensions:')
+
         for filename in listdir('./lib/extensions'): #loads all files (*.py)
             if filename.endswith('.py'):
-                self.load_extension(f'lib.extensions.{filename[:-3]}') #loads the file without ".py" 
+                self.load_extension(f'lib.extensions.{filename[:-3]}') # [:-3] to remove '.py' 
                 print(f'> Loaded {green}{filename[:-3]}{reset} from lib.extensions.{filename[:-3]}')
