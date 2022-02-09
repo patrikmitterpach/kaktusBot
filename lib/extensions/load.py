71ba9743f695f4fe3fadb10cdb68a5789d6b2db0
Mitterpach <patrik@mitterpach.com>
2022-02-09 16:19:53 +0100

    Add varied dir/module name support for `load.py`

--- lib/extensions/load.py
+++ lib/extensions/load.py
@@ -1,18 +1,19 @@
 from glob import glob
 
 def fileToModule(file):
-    return file.split('/')[-1] # returns the last file in a listing
+    path = file.split('/')
+    return path[-2], path[-1] # returns the last file in a listing
 
-def discordModulePath(module):
-    return f'lib.extensions.{ module[:-3] }.{ module[:-3] }'
+def discordModulePath(folder, module):
+    return f'lib.extensions.{ folder }.{ module[:-3] }'
 
 def findExtensions(dir):
     extensionList = []
     
     files = glob('./lib/extensions/*/*.py')
     for file in files:
-        module = fileToModule(file)                 # *.py           
-        discordPath = discordModulePath(module)     # lib.extensions.*.*
+        folder, module = fileToModule(file)                 # *.py           
+        discordPath = discordModulePath(folder, module)     # lib.extensions.*.*
 
         extensionList.append( discordPath )
     return extensionList
