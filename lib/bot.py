70e2dd6c06f99a45a2d3837cb0628430cfdc4757
Mitterpach <patrik@mitterpach.com>
2022-02-09 15:09:39 +0100

    Increase visual clarity for `bot.py`

--- lib/bot.py
+++ lib/bot.py
@@ -3,6 +3,7 @@ from glob import glob
 
 import discord
 from discord.ext import commands
+from .extensions import load 
 
 currentGame   = 'Detroit: Become Human'
 currentStatus = 'idle'
@@ -12,6 +13,9 @@ commandPrefix = '~'
 yellow = '\033[33m'
 green = '\033[32m'
 reset = '\033[m'
+
+def logExtension(extension):
+    print(f'> Loaded {green}{extension}{reset}')
 class Bot(commands.Bot):
     async def on_ready(self):
         print(f'Connected as {yellow}{self.user}{reset}')
@@ -20,14 +24,14 @@ class Bot(commands.Bot):
     def create(cls):
         return cls(
                 command_prefix = commandPrefix,
-                activity = discord.Game(currentGame),
-                status = currentStatus,
-                help_command=None )
+                activity       = discord.Game(currentGame),
+                status         = currentStatus,
+                help_command   = None )
         
     def loadExtensions(self):
         print('Loading extensions:')
+        extensions = load.findExtensions('./lib/extensions/*/*.py')
 
-        for filename in glob('./lib/extensions/*/*.py'):
-            module = filename.split('/')[-1] #loads all files (*.py)
-            self.load_extension(f'lib.extensions.{module[:-3]}.{module[:-3]}') # [:-3] to remove '.py' 
-            print(f'> Loaded {green}{module[:-3]}{reset} from lib.extensions.{module[:-3]}.')
+        for extension in extensions:
+            self.load_extension(extension) 
+            logExtension(extension)
